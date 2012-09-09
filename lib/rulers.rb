a47171489346e524714a2ea4e746eb1243bdcc7e
Sean Liu <seanliugm@gmail.com>
2012-09-09 13:34:23 +0800

    exercises in chapter 1

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,9 +1,14 @@
 require "rulers/version"
+#for exercise 2
+require 'rulers/array'
 
 module Rulers
   # Your code goes here...
   class Application
     def call(env)
+     #for exercise 1
+     # `echo debug > debug.txt`
+      puts [1, 2].sum
       [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
     end
   end
